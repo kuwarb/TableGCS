4ce92280596fbdd50b297f419a4ba3a94496cd2a
kuwar.bagga <kuwar.bagga@shl.com>
2020-10-10 01:06:16 +0530

    Api call

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
-const http = require("http");
+const https = require("https");
 const app = require("./app");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
 
 const port = process.env.PORT || 3001;
 
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 
 server.listen(port);
--- src/componenets/Table.js
+++ src/componenets/Table.js
@@ -1,70 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const heading = ["Id", "Name", "Status", "Description", "Delta", "CreatedOn"];
 
 const Table = (props) => {
+  const [tableData, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://localhost:3001/pagination/20/1").then((response) => {
+      console.log("Response", response);
+      setData(response.data.data);
+    });
+  }, []);
+
   return (
-    <table class="container">
-      <thead>
-        <tr>
-          {heading.map((thead) => (
-            <th>
-              <h1>{thead}</h1>
-            </th>
+    <>
+      <p>Name Search</p>
+      <input type="text"></input>
+      <table className="container">
+        <thead>
+          <tr>
+            {heading.map((thead, index) => (
+              <th key={index}>
+                <h1>{thead}</h1>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.status}</td>
+              <td>{item.description}</td>
+              <td>{item.delta}</td>
+              <td>{item.createdOn}</td>
+            </tr>
           ))}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Google</td>
-          <td>9518</td>
-          <td>6369</td>
-          <td>01:32:50</td>
-          <td>6369</td>
-          <td>01:32:50</td>
-        </tr>
-        <tr>
-          <td>Twitter</td>
-          <td>7326</td>
-          <td>10437</td>
-          <td>00:51:22</td>
-          <td>6369</td>
-          <td>01:32:50</td>
-        </tr>
-        <tr>
-          <td>Amazon</td>
-          <td>4162</td>
-          <td>5327</td>
-          <td>00:24:34</td>
-          <td>6369</td>
-          <td>01:32:50</td>
-        </tr>
-        <tr>
-          <td>LinkedIn</td>
-          <td>3654</td>
-          <td>2961</td>
-          <td>00:12:10</td>
-          <td>6369</td>
-          <td>01:32:50</td>
-        </tr>
-        <tr>
-          <td>CodePen</td>
-          <td>2002</td>
-          <td>4135</td>
-          <td>00:46:19</td>
-          <td>6369</td>
-          <td>01:32:50</td>
-        </tr>
-        <tr>
-          <td>GitHub</td>
-          <td>4623</td>
-          <td>3486</td>
-          <td>00:31:52</td>
-          <td>6369</td>
-          <td>01:32:50</td>
-        </tr>
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </>
   );
 };
 
